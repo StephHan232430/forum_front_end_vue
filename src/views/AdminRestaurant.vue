7ad4e95980efb029ddac339f3333a61d02f06c8d
Steph <blackman914037@gmail.com>
2020-03-05 00:45:42 +0800

    Admin getRestaurant API connection

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -41,27 +41,9 @@
 </template>
 <script>
 import { emptyImageFilter } from './../utils/mixins'
-const dummyData = {
-  restaurant: {
-    id: 2,
-    name: 'Mrs. Mckenzie Johnston',
-    tel: '567-714-6131 x621',
-    address: '61371 Rosalinda Knoll',
-    opening_hours: '08:00',
-    description:
-      'Quia pariatur perferendis architecto tenetur omnis pariatur tempore.',
-    image: 'https://loremflickr.com/320/240/food,dessert,restaurant/?random=2',
-    createdAt: '2019-06-22T09:00:43.000Z',
-    updatedAt: '2019-06-22T09:00:43.000Z',
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  }
-}
+import adminAPI from '../apis/admin'
+import { Toast } from '../utils/helpers'
+
 export default {
   name: 'AdminRestaurant',
   mixins: [emptyImageFilter],
@@ -83,20 +65,44 @@ export default {
     const { id: restaurantId } = this.$route.params
     this.fetchRestaurant(restaurantId)
   },
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params
+    this.fetchRestaurant(id)
+  },
   methods: {
-    fetchRestaurant(restaurantId) {
-      const { restaurant } = dummyData
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryName: restaurant.Category.name,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description
+    async fetchRestaurant(restaurantId) {
+      try {
+        // const response = await adminAPI.restaurants.getDetail({ restaurantId })
+        // console.log(response)
+        const { data: { restaurant }, statusText } = await adminAPI.restaurants.getDetail({ restaurantId })
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          ...restaurant,
+          categoryName: restaurant.Category.name,
+          openingHours: restaurant.opening_hours
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法顯示餐廳詳細資訊，請稍後再試'
+        })
       }
+
+      // const { restaurant } = dummyData
+      // this.restaurant = {
+      //   ...this.restaurant,
+      //   id: restaurant.id,
+      //   name: restaurant.name,
+      //   categoryName: restaurant.Category.name,
+      //   image: restaurant.image,
+      //   openingHours: restaurant.opening_hours,
+      //   tel: restaurant.tel,
+      //   address: restaurant.address,
+      //   description: restaurant.description
+      // }
     }
   }
 }
