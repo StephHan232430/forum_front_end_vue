909132d4282bdc2c8347c05bdf20e7b9f65557de
Steph <blackman914037@gmail.com>
2020-03-04 19:47:50 +0800

    Admin createRestaurant, getRestaurants and deleteRestaurant API connection

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+    <AdminRestaurantForm
+      :is-processing="isProcessing"
+      @after-submit="handleAfterSubmit"
+    />
   </div>
 </template>
 
 <script>
 import AdminRestaurantForm from '../components/AdminRestaurantForm'
+import adminAPI from '../apis/admin'
+import { Toast } from '../utils/helpers'
 
 export default {
   components: {
     AdminRestaurantForm
   },
+  data() {
+    return {
+      isProcessing: false
+    }
+  },
   methods: {
-    handleAfterSubmit(formData) {
-      for (const [name, value] of formData.entries()) {
-        console.log(name + ': ' + value)
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true
+        const { data, statusText } = await adminAPI.restaurants.create({ formData })
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
+        }
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法建立餐廳，請稍後再試'
+        })
       }
     }
   }
