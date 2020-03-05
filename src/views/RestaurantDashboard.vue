1fce41ec884c8d3e0579a5001594fa0c4b211e4c
Steph <blackman914037@gmail.com>
2020-03-06 04:22:15 +0800

    RestaurantDahboard API connection

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -3,11 +3,11 @@
     <div class="row">
       <div class="col-md-12">
         <h1>{{ restaurant.name }}</h1>
-        <p>[{{ restaurant.Category.name }}]</p>
+        <p>[{{ restaurant.categoryName }}]</p>
         <hr>
         <ul>
-          <li>有 {{ restaurant.Comments.length }} 筆評論</li>
-          <li>有 {{ restaurant.FavoritedUsers.length }} 人收藏這家餐廳</li>
+          <li>有 {{ restaurant.comments.length }} 筆評論</li>
+          <li>有 {{ restaurant.favoritedUsers.length }} 人收藏這家餐廳</li>
         </ul>
         <br />
         <br />
@@ -19,105 +19,44 @@
 </template>
 
 <script>
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: 'Dr. Izaiah Ernser',
-    tel: '389.061.3241 x162',
-    address: '7226 Johns Islands',
-    opening_hours: '08:00',
-    description: 'quam',
-    image: 'https://loremflickr.com/320/240/restaurant,food/?random=90.45122643685453',
-    viewCounts: 2,
-    createdAt: '2020-02-28T14:38:32.000Z',
-    updatedAt: '2020-03-01T03:00:40.000Z',
-    CategoryId: 2,
-    Category: {
-      id: 2,
-      name: '日本料理',
-      createdAt: '2020-02-28T14:38:32.000Z',
-      updatedAt: '2020-02-28T14:38:32.000Z'
-    },
-    Comments: [
-      {
-        id: 1,
-        text: 'Dolores dolorem maxime debitis blanditiis quam sed voluptatem mollitia.',
-        UserId: 3,
-        RestaurantId: 1,
-        createdAt: '2020-02-28T14:38:32.000Z',
-        updatedAt: '2020-02-28T14:38:32.000Z',
-        User: {
-          id: 3,
-          name: 'user2',
-          email: 'user2@example.com',
-          password: '$2a$10$VHKmtPqbcUzK46qxLllqj.w506U2N2TObMmnpdlNG2CLZPa1xzuTi',
-          isAdmin: false,
-          image: null,
-          createdAt: '2020-02-28T14:38:32.000Z',
-          updatedAt: '2020-02-28T14:38:32.000Z'
-        }
-      },
-      {
-        id: 51,
-        text: 'Nobis aspernatur nobis cumque sed nemo et autem.',
-        UserId: 2,
-        RestaurantId: 1,
-        createdAt: '2020-02-28T14:38:32.000Z',
-        updatedAt: '2020-02-28T14:38:32.000Z',
-        User: {
-          id: 2,
-          name: 'user1',
-          email: 'user1@example.com',
-          password: '$2a$10$NyaAtgRuHx3i7hHlnb5IXOC4Uk4.q1J1iQs3op.ymdCEh7.tOwcH2',
-          isAdmin: false,
-          image: null,
-          createdAt: '2020-02-28T14:38:32.000Z',
-          updatedAt: '2020-02-28T14:38:32.000Z'
-        }
-      },
-      {
-        id: 101,
-        text: 'Non nesciunt eveniet pariatur sit id officia consequuntur.',
-        UserId: 1,
-        RestaurantId: 1,
-        createdAt: '2020-02-28T14:38:32.000Z',
-        updatedAt: '2020-02-28T14:38:32.000Z',
-        User: {
-          id: 1,
-          name: 'root',
-          email: 'root@example.com',
-          password: '$2a$10$J9pLpJJ1Tzfe/ZcjdYwXdumyh.3F5E.w/HTxRcH./cl3azhgekgQe',
-          isAdmin: true,
-          image: null,
-          createdAt: '2020-02-28T14:38:32.000Z',
-          updatedAt: '2020-02-28T14:38:32.000Z'
-        }
-      }
-    ],
-    FavoritedUsers: [
-      {
-        id: 1,
-        name: 'root',
-        email: 'root@example.com',
-        password: '$2a$10$J9pLpJJ1Tzfe/ZcjdYwXdumyh.3F5E.w/HTxRcH./cl3azhgekgQe',
-        isAdmin: true,
-        image: null,
-        createdAt: '2020-02-28T14:38:32.000Z',
-        updatedAt: '2020-02-28T14:38:32.000Z',
-        Favorite: {
-          UserId: 1,
-          RestaurantId: 3,
-          createdAt: '2020-02-29T16:54:54.000Z',
-          updatedAt: '2020-02-29T16:54:54.000Z'
-        }
-      }
-    ]
-  }
-}
+import restaurantsAPI from '../apis/restaurants'
+import { Toast } from '../utils/helpers'
 export default {
   data() {
     return {
-      restaurant: dummyData.restaurant
+      restaurant: {
+        name: '',
+        categoryName: '',
+        comments: [],
+        favoritedUsers: []
+      }
+    }
+  },
+  created() {
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
+  },
+  methods: {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data, statusText } = await restaurantsAPI.getRestaurant({ restaurantId })
+        console.log({ data, statusText })
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          name: data.restaurant.name,
+          categoryName: data.restaurant.Category.name,
+          comments: data.restaurant.Comments,
+          favoritedUsers: data.restaurant.FavoritedUsers
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳報表，請稍後再試'
+        })
+      }
     }
   }
 }
