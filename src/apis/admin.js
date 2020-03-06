1e8998c609d8affc87e093103d1a56fe9eed6e5f
Steph <blackman914037@gmail.com>
2020-03-06 17:11:32 +0800

    Refactor adminAPI

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,54 +1,35 @@
 import { apiHelper } from '../utils/helpers'
-const getToken = () => localStorage.getItem('token')
 
 export default {
   categories: {
     get() {
-      return apiHelper.get('/admin/categories', {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.get('/admin/categories')
     },
     delete({ categoryId }) {
-      return apiHelper.delete(`/admin/categories/${categoryId}`, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.delete(`/admin/categories/${categoryId}`)
     },
     update({ categoryId, name }) {
-      return apiHelper.put(`/admin/categories/${categoryId}`, { categoryId, name }, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.put(`/admin/categories/${categoryId}`, { categoryId, name })
     },
     create({ name }) {
-      return apiHelper.post('/admin/categories', { name }, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.post('/admin/categories', { name })
     }
   },
   restaurants: {
     create({ formData }) {
-      return apiHelper.post('/admin/restaurants', formData, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.post('/admin/restaurants', formData)
     },
     get() {
-      return apiHelper.get('/admin/restaurants', {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.get('/admin/restaurants')
     },
     delete({ restaurantId }) {
-      return apiHelper.delete(`/admin/restaurants/${restaurantId}`, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.delete(`/admin/restaurants/${restaurantId}`)
     },
     getDetail({ restaurantId }) {
-      return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.get(`/admin/restaurants/${restaurantId}`)
     },
     update({ restaurantId, formData }) {
-      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, {
-        headers: { Authorization: `Bearer ${getToken()}` }
-      })
+      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData)
     }
   }
 }
