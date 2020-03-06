f738254e9ba9f95a9d3e96f719cfbd467cedcc2e
Steph <blackman914037@gmail.com>
2020-03-06 16:33:51 +0800

    Update user profile editing, and refactor usersAPI

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -1,40 +1,61 @@
 <template>
   <div class="container py-5">
-    <!-- UserForm -->
-    <UserForm
-      :initial-profile="profile"
-      @after-submit="handleAfterSubmit"
-    />
+    <form @submit.stop.prevent="handleSubmit">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input
+          id="name"
+          v-model="name"
+          type="text"
+          class="form-control"
+          name="name"
+          placeholder="Enter name"
+          required
+        >
+      </div>
+
+      <div class="form-group">
+        <label for="image">Image</label>
+
+        <img
+          v-if="image"
+          :src="image"
+          class="d-block img-thumbnail mb-3"
+          width="200"
+          height="200"
+        >
+
+        <input
+          id="image"
+          type="file"
+          name="image"
+          accept="image/*"
+          class="form-control-file"
+          @change="handleFileChange"
+        >
+      </div>
+
+      <button
+        type="submit"
+        class="btn btn-primary"
+      >
+        送出
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
-import UserForm from '../components/UserForm'
-const dummyUser = {
-  profile: {
-    id: 1,
-    name: 'root',
-    email: 'root@example.com',
-    password: '$2a$10$J9pLpJJ1Tzfe/ZcjdYwXdumyh.3F5E.w/HTxRcH./cl3azhgekgQe',
-    isAdmin: true,
-    image: 'https://i.imgur.com/58ImzMM.png'
-  }
-}
+import usersAPI from '../apis/users'
+import { Toast } from '../utils/helpers'
 
 export default {
-  components: {
-    UserForm
-  },
   data() {
     return {
-      profile: {
-        id: -1,
-        name: '',
-        email: '',
-        password: '',
-        isAdmin: false,
-        image: ''
-      }
+      id: -1,
+      name: '',
+      email: '',
+      image: ''
     }
   },
   created() {
@@ -42,16 +63,44 @@ export default {
     this.fetchUser(id)
   },
   methods: {
-    fetchUser(id) {
-      console.log('fetchUser id: ', id)
-      this.profile = dummyUser.profile
-    },
-    handleAfterSubmit(formData) {
-      const { id } = this.$route.params
-      for (const [name, value] of formData.entries()) {
-        console.log(name + ': ' + value)
+    async fetchUser(userId) {
+      try {
+        const { data, statusText } = await usersAPI.get({ userId })
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+        this.id = data.profile.id
+        this.name = data.profile.name
+        this.email = data.profile.email
+        this.image = data.profile.image
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法編輯使用者資訊，請稍後再試'
+        })
       }
-      this.$router.push(`/users/${id}`)
+    },
+    handleFileChange(e) {
+      const files = e.target.files
+      if (!files.length) return
+      const imageURL = window.URL.createObjectURL(files[0])
+      this.image = imageURL
+    },
+    async handleSubmit(e) {
+      try {
+        const form = e.target
+        const formData = new FormData(form)
+        const { data, statusText } = await usersAPI.update({ userId: this.id, formData })
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
+        }
+        this.$router.push({ name: 'user-profile', params: { id: this.id } })
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法更新使用者，請稍後再試'
+        })
+      }
     }
   }
 }
