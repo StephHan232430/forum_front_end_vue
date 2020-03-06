89f6153a2dc1b4294c4d153b3eebcea3ecdfe64a
Steph <blackman914037@gmail.com>
2020-03-06 17:09:41 +0800

    Refactor commentsAPI

--- src/apis/comments.js
+++ src/apis/comments.js
@@ -1,17 +1,13 @@
 import { apiHelper } from '../utils/helpers'
-const getToken = () => localStorage.getItem('token')
 
 export default {
   addComment({ restaurantId, text }) {
     return apiHelper.post(
       '/comments',
-      { restaurantId, text },
-      { headers: { Authorization: `Bearer ${getToken()}` } }
+      { restaurantId, text }
     )
   },
   deleteComment({ commentId }) {
-    return apiHelper.delete(`/comments/${commentId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.delete(`/comments/${commentId}`)
   }
 }
