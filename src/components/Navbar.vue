69c8e76388c1a13f69cac35e5f56af4e7f2fb03c
Steph <blackman914037@gmail.com>
2020-03-06 15:02:24 +0800

    Upadate store and router settings

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -45,6 +45,7 @@
           <button
             type="button"
             class="btn btn-sm btn-outline-success my-2 my-sm-0"
+            @click.stop.prevent="logout"
           >
             登出
           </button>
@@ -60,6 +61,12 @@ export default {
   name: 'Navbar',
   computed: {
     ...mapState(['currentUser', 'isAuthenticated'])
+  },
+  methods: {
+    logout() {
+      this.$store.commit('revokeAuthentication')
+      this.$router.push('/signin')
+    }
   }
 }
 </script>
